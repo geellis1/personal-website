cc94e76e50649ce693b5ef7d69c448460bf94a60
Gradi Ellis <[ellis.gradi@gmail.com]>
2020-01-09 15:47:51 -0600

    Updates

---
+++ precache-manifest.1fa32e95919fe490bb3199e58de87224.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2b31b76c9dad23a0c3ecf707ea4e8e",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "b6086b4c1885969d5a3e",
+    "url": "/personal-website/static/css/main.4dbf61fb.chunk.css"
+  },
+  {
+    "revision": "3d5f466a658e8fdf2a77",
+    "url": "/personal-website/static/js/2.54f9668f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/personal-website/static/js/2.54f9668f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6086b4c1885969d5a3e",
+    "url": "/personal-website/static/js/main.8ac1fb94.chunk.js"
+  },
+  {
+    "revision": "1290f057b8656b4605f4",
+    "url": "/personal-website/static/js/runtime-main.bb8d2403.js"
+  },
+  {
+    "revision": "47a61e969539300d7a3fa7a3a6522163",
+    "url": "/personal-website/static/media/UXUI-01.47a61e96.png"
+  },
+  {
+    "revision": "1257919cc24e3c0297a9c0472db6e1d6",
+    "url": "/personal-website/static/media/code-01.1257919c.png"
+  },
+  {
+    "revision": "8c0cf96fa9ed17ad1516026cae7030bb",
+    "url": "/personal-website/static/media/gradilogo.8c0cf96f.png"
+  },
+  {
+    "revision": "169522134595b1e002d2ceeebdb61bfe",
+    "url": "/personal-website/static/media/graphicdesign-01.16952213.png"
+  },
+  {
+    "revision": "dee61fd9614aafb9ca872ad88335d8fa",
+    "url": "/personal-website/static/media/portfolioheadshot.dee61fd9.png"
+  },
+  {
+    "revision": "54cc5166248c62d98c98746b2f249f36",
+    "url": "/personal-website/static/media/portfoliolanding.54cc5166.png"
+  }
+]);
